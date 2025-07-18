2cd0a0213539de20bba3277bb89d47cce35b2658
YuuuuuRAE <127469346+YuuuuuRAE@users.noreply.github.com>
2025-07-18 21:15:34 +0900

    feat: Implement player move

---
+++ Enough_Oxygen/Assets/00. Scripts/Player/PlayerMove.cs
@@ -0,0 +1,29 @@
+﻿// Unity
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+
+    private Rigidbody2D rigid;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        // Move
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector2 inputVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 moveVector = inputVector.normalized * speed * Time.fixedDeltaTime;
+
+        rigid.MovePosition(rigid.position + moveVector);
+    }
+}
