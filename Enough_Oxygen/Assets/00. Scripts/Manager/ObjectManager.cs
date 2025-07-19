89eb3ea735fc26275f8cc22af355e948f86e9446
YuuuuuRAE <127469346+YuuuuuRAE@users.noreply.github.com>
2025-07-20 08:30:28 +0900

    fix: Fix resolution

--- Enough_Oxygen/Assets/00. Scripts/Manager/ObjectManager.cs
+++ Enough_Oxygen/Assets/00. Scripts/Manager/ObjectManager.cs
@@ -9,11 +9,15 @@ public class ObjectManager : MonoBehaviour
 
     [SerializeField] private UIManager uiManager;
 
+    private bool isClear = false;
+
     public void CompleteSecurityMission()
     {
         GameManager.Data.data.clearCount++;
         securityObject.MissionComplete();
         uiManager.CheckToDoList(0);
+
+        if (GameManager.Data.data.clearCount == 3) GameClear();
     }
 
     public void CompleteCommunicationMission()
@@ -21,6 +25,8 @@ public class ObjectManager : MonoBehaviour
         GameManager.Data.data.clearCount++;
         communicationObject.MissionComplete();
         uiManager.CheckToDoList(1);
+
+        if (GameManager.Data.data.clearCount == 3) GameClear();
     }
 
     public void CompleteExitMission()
@@ -28,5 +34,28 @@ public class ObjectManager : MonoBehaviour
         GameManager.Data.data.clearCount++;
         exitObject.MissionComplete();
         uiManager.CheckToDoList(2);
+
+        if (GameManager.Data.data.clearCount == 3) GameClear();
+    }
+
+    public void GameClear()
+    {
+        Player player = FindAnyObjectByType<Player>();
+        if (player != null) player.gameObject.SetActive(false);
+
+        TimeManager timeManager = FindObjectOfType<TimeManager>();
+        if (timeManager != null) timeManager.Pause();
+
+        isClear = true;
+
+        Instantiate(GameManager.Resource.Load<Canvas>("Prefabs/UI", "Game Clear"));
+    }
+
+    private void Update()
+    {
+        if (isClear && Input.GetKeyDown(KeyCode.Escape))
+        {
+            GameManager.Scene.ConvertScene("Main Title");
+        }
     }
 }
